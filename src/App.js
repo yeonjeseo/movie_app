9bd6adb2c09a6351f8be5c6bdce1a9dde200101c
yeonjeseo <yeonjeseo@hotmail.com>
2020-04-01 23:26:36 +0900

    React fundamental

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+function Food({favorite}) {
+    return <h1>I like {favorite}!!</h1>;
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            Hello!!!
+            <Food favorite='kimchi' />
+            <Food favorite='samgiopsal' />
+            <Food favorite='ramen' />
+            <Food favorite='chukumi' />
+        </div>
+    );
 }
 
 export default App;
