544f7e398f7514be8c8045089443898e40fcc0b4
yeonjeseo <yeonjeseo@hotmail.com>
2020-04-03 14:19:17 +0900

    Rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
     state = {
-        isLoading: true
+        isLoading: true,
+        movies: []
     };
 
+    getMovies = async () => {
+        const {
+            data: {
+                data: { movies }
+            }
+        } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=ratings");
+        console.log(movies);
+        // this.setState({ movies: movies });
+        this.setState({ movies, isLoading: false });
+    };
     componentDidMount() {
-        setTimeout( () => {
-            this.setState({ isLoading: false, Ryuha: true });
+        this.getMovies();
+
+        setTimeout(() => {
             console.log(this.state);
-        }, 6000);
+        }, 2000);
     }
 
     render() {
-        console.log("this.state");
         // C++ using namespace 사용과 비슷한 듯
-        const { isLoading } = this.state;
-        return <div>{isLoading ? "Loading" : "We are ready"};</div>;
+        const { isLoading, movies } = this.state;
+        return (
+            <div>
+                {isLoading
+                    ? "Loading"
+                    : movies.map(movie =>(
+                          <Movie
+                            key={movie.id} 
+                            id={movie.id} 
+                            year={movie.year} 
+                            title={movie.title} 
+                            summary={movie.summary} 
+                            poster={movie.medium_cover_image} />
+                      ))}
+            </div>
+        );
     }
 }
 
