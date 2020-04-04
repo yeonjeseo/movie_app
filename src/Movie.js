c782583e09dd829ae1606bd795906a7b73031de0
yeonjeseo <yeonjeseo@hotmail.com>
2020-04-05 01:40:14 +0900

    4.4 Styles Timelapse, issue with the container

--- src/Movie.js
+++ src/Movie.js
@@ -4,19 +4,17 @@ import "./Movie.css";
 
 function Movie({ year, title, summary, poster, genres }) {
     return (
-        <div className="movie">
+        <div className='movie'>
             <img src={poster} alt={title} title={title} />
-            <div className="movie__data">
-                <h3 className="movie__title">{title}</h3>
-                <h5 className="movie__year">{year}</h5>
-                <ul className="movie__genre">
-                    {genres.map(
-                        (genre, index)=>(
-                            <li key={index} className='genres__genre'>
-                                {genre}
-                            </li>
-                        )
-                    )}
+            <div className='movie__data'>
+                <h3 className='movie__title'>{title}</h3>
+                <h5 className='movie__year'>{year}</h5>
+                <ul className='movie__genres'>
+                    {genres.map((genre, index) => (
+                        <li key={index} className='genres__genre'>
+                            {genre}
+                        </li>
+                    ))}
                 </ul>
                 <p className='movie__summary'>{summary}</p>
             </div>
@@ -30,7 +28,7 @@ Movie.propTypes = {
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
-    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
