5f7b55a509fe363b0bfba87e76b0bf4d14db66c3
yeonjeseo <yeonjeseo@hotmail.com>
2020-04-04 16:18:29 +0900

    4.4 Styles Timelapse, unfinishedgit add . sibalgit add .

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
     state = {
         isLoading: true,
-        movies: []
+        movies: [],
     };
 
     getMovies = async () => {
         const {
             data: {
-                data: { movies }
-            }
+                data: { movies },
+            },
         } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=ratings");
         console.log(movies);
         // this.setState({ movies: movies });
@@ -31,20 +32,13 @@ class App extends React.Component {
         const { isLoading, movies } = this.state;
         return (
             <section className="container">
-                {isLoading
-                    ? <div className="loader">
-                        <span className="loader_text">Loading.....</span>
+                {isLoading ? (
+                    <div className='loader'>
+                        <span className='loader_text'>Loading.....</span>
                     </div>
-                    : movies.map(movie =>(
-                          <Movie
-                            key={movie.id} 
-                            id={movie.id} 
-                            year={movie.year} 
-                            title={movie.title} 
-                            summary={movie.summary} 
-                            poster={movie.medium_cover_image}
-                            genres={movie.genres} />
-                      ))}
+                ) : (
+                    movies.map((movie) => <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres} />)
+                )}
             </section>
         );
     }
--- src/Movie.js
+++ src/Movie.js
@@ -4,12 +4,12 @@ import "./Movie.css";
 
 function Movie({ year, title, summary, poster, genres }) {
     return (
-        <div className='movie'>
+        <div className="movie">
             <img src={poster} alt={title} title={title} />
-            <div className='movie__data'>
-                <h3 className='movie__title'>{title}</h3>
-                <h3 className='movie__year'>{year}</h3>
-                <ul className='movie__genre'>
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="movie__genre">
                     {genres.map(
                         (genre, index)=>(
                             <li key={index} className='genres__genre'>
