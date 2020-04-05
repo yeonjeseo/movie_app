cfaff85c349d631e35c5b75649ccfa6afddd408f
yeonjeseo <yeonjeseo@hotmail.com>
2020-04-06 00:10:51 +0900

    Updates

---
+++ precache-manifest.ea8ce2d47b73d90fc9786bc63adc4b06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7b8eb3b70c5e73f4b070356e16e652",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "d26dfe826623f062ff8f",
+    "url": "/movie_app/static/css/main.56d47750.chunk.css"
+  },
+  {
+    "revision": "1d2d5261e5e33a966d0f",
+    "url": "/movie_app/static/js/2.111c2b99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.111c2b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d26dfe826623f062ff8f",
+    "url": "/movie_app/static/js/main.3671c979.chunk.js"
+  },
+  {
+    "revision": "c4e503dc5fc5f4a3fb01",
+    "url": "/movie_app/static/js/runtime-main.202e151f.js"
+  }
+]);
