06e6332f2bef46486f4f92b1f3109bac9432c2e8
yeonjeseo <yeonjeseo@hotmail.com>
2020-04-07 13:01:06 +0900

    6

--- src/App.js
+++ src/App.js
@@ -1,47 +1,20 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
+import {HashRouter, Route} from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Navigation from "./components/Navigation"
 import "./App.css";
 
-class App extends React.Component {
-    state = {
-        isLoading: true,
-        movies: [],
-    };
+function App() {
+    return  <HashRouter>
+        <Navigation />
+        <Route path="/home">
 
-    getMovies = async () => {
-        const {
-            data: {
-                data: { movies },
-            },
-        } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=ratings");
-        console.log(movies);
-        // this.setState({ movies: movies });
-        this.setState({ movies, isLoading: false });
-    };
-    
-    componentDidMount() {
-        this.getMovies();
-    }
+        </Route>
+        <Route path="/about">
 
-    render() {
-        // C++ using namespace 사용과 비슷한 듯
-        const { isLoading, movies } = this.state;
-
-        return (
-            <section className="container">
-                <div className="movies">
-                {isLoading ? (
-                    <div className='loader'>
-                        <span className='loader_text'>Loading.....</span>
-                    </div>
-                ) : (
-                    movies.map((movie) => <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres} />)
-                    )}
-                </div>
-            </section>
-        );
-    }
+        </Route>
+    </HashRouter>
 }
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./About.css";
+
+
+function About() {
+    return <span>About this page : I built it beacuse I love movies</span>
+}
+
+export default About;
